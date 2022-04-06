cdf9fe12a0771368ad4e30ea217e191fbc80e21f
Thun (Mac Mini) <bsthun@gmail.com>
2022-04-06 21:07:44 +0700

    Add model

---
+++ client/lib/models/README.md
@@ -0,0 +1,4 @@
+# Models
+Contains the data object and DTO for the project data flow.
+
+For model the object class, please see: https://stackoverflow.com/questions/54703549/how-to-store-response-to-object-in-flutter-using-dio
---
+++ client/lib/models/folder.dart
@@ -0,0 +1,27 @@
+class Folder {
+  int id;
+  String name;
+  int count;
+
+  Folder({
+    required this.id,
+    required this.name,
+    required this.count,
+  })
+
+  factory Folder.fromJson(Map<String, dynamic> json) {
+    return Folder(
+        id: json["id"],
+        name: json["name"],
+        count: json["count"]
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "name": name,
+      "count": count,
+    };
+  }
+}
