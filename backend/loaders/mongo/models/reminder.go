0faf978a2abe2d4ab98a50d959f59143782de002
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-04-21 17:31:28 +0700

    update: reminder schema

--- backend/loaders/mongo/models/reminder.go
+++ backend/loaders/mongo/models/reminder.go
@@ -10,6 +10,7 @@ type Reminder struct {
 	mgm.DefaultModel `bson:",inline"`
 	UserId           *uint64    `json:"user_id" bson:"user_id"`
 	Title            *string    `json:"title" bson:"title"`
+	HasNote          *bool      `bson:"has_note" bson:"has_note"`
 	Description      *string    `json:"description" bson:"description"`
 	RemindDate       *time.Time `json:"remind_date" bson:"remind_date"`
 	RemindTime       *time.Time `json:"remind_time" bson:"remind_time"`
