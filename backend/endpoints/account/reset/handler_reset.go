cf31acf291ff0e1b0968aad8653e1bf48e0a5b05
Apisit Maneerat <apisit.mixko@mail.kmutt.ac.th>
2022-05-16 03:04:49 +0700

    update: verify pin and change password endpoints

--- backend/endpoints/account/reset/handler_reset.go
+++ backend/endpoints/account/reset/handler_reset.go
@@ -5,26 +5,26 @@ import (
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/bson"
 	mongoDriver "go.mongodb.org/mongo-driver/mongo"
-
 	"noty-backend/loaders/mongo/models"
 	"noty-backend/types/responder"
+	"noty-backend/utils/crypto"
 	"noty-backend/utils/text"
 )
 
-// VerifyHandler
-// @ID account.reset.verify.post
-// @Summary Reset verify pin
-// @Description Reset verify pin
+// ResetHandler
+// @ID account.reset.post
+// @Summary Reset account password
+// @Description Reset account password
 // @Tags account
 // @Accept json
 // @Produce json
-// @Param payload body verifyRequest true "reset.verifyRequest"
+// @Param payload body resetPasswordRequest true "reset.resetPasswordRequest"
 // @Success 200 {object} responder.InfoResponse
 // @Failure 400 {object} responder.ErrorResponse
-// @Router /account/reset/verify [post]
-func VerifyHandler(c *fiber.Ctx) error {
+// @Router /account/reset/password [post]
+func ResetHandler(c *fiber.Ctx) error {
 	// * Parse body
-	var body verifyRequest
+	var body resetPasswordRequest
 	if err := c.BodyParser(&body); err != nil {
 		return &responder.GenericError{
 			Message: "Unable to parse body",
@@ -37,15 +37,23 @@ func VerifyHandler(c *fiber.Ctx) error {
 		return err
 	}
 
-	// * Clear verify pin
+	// * Hash new password
+	password, err := crypto.HashPassword(body.Password)
+	if err != nil {
+		return &responder.GenericError{
+			Message: "Unable to hash the password",
+			Err:     err,
+		}
+	}
+
+	// * Update password
 	if result := mgm.Coll(new(models.User)).FindOneAndUpdate(
 		mgm.Ctx(),
 		bson.M{
-			"email":       body.Email,
-			"reset_token": body.Pin,
+			"email": body.Email,
 		},
 		bson.M{"$set": bson.M{
-			"reset_token": nil,
+			"password": password,
 		}},
 	); result.Err() == mongoDriver.ErrNoDocuments {
 		return &responder.GenericError{
@@ -60,6 +68,6 @@ func VerifyHandler(c *fiber.Ctx) error {
 
 	return c.JSON(&responder.InfoResponse{
 		Success: true,
-		Info:    "Your verify pin has been reset",
+		Info:    "Your password reset has been proceed, you can now login to the app",
 	})
 }
